5a96076041fbf26585c67dc97526dc5be7f7c8a8
billy david <williamdavid423@hotmail.com>
2021-03-08 14:13:34 -0500

    finished reference

--- Graphic examples/Form1.cs
+++ Graphic examples/Form1.cs
@@ -22,6 +22,21 @@ namespace Graphic_examples
             Graphics g = this.CreateGraphics();
             Pen redPen = new Pen(Color.Red, 10);
             SolidBrush blackBrush = new SolidBrush(Color.Black);
+            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
+            
+
+            g.Clear(Color.White);
+            g.DrawLine(redPen, 0, 0, 100, 100);
+            g.DrawRectangle(redPen, 30, 30, 100, 200);
+            g.FillRectangle(blackBrush, 30, 30, 100, 200);
+
+            g.DrawEllipse(redPen, 25, 25, 200, 100);
+            g.FillEllipse(blackBrush, 25, 25, 200, 100);
+
+            g.DrawArc(redPen, 250, 20, 50, 50, 1200, 300);
+            g.DrawPie(redPen, 250, 100, 50, 50, 30, 300);
+
+            g.DrawString("Hello", drawFont, blackBrush, 50, 40);
         }
     }
 }
