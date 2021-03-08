04f3ec6d1ba7944d55f591c71222412ed441df2a
billy david <williamdavid423@hotmail.com>
2021-03-08 13:54:32 -0500

    Add project files.

---
+++ Graphic examples/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphic_examples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen redPen = new Pen(Color.Red, 10);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+        }
+    }
+}
